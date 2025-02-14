ce0e16ceb3574e42b8c081144d7f88552888bd84
HRC-Manu <manuel.weiss@hr-campus.ch>
2025-02-14 23:05:56 +0100

    Kompatibilität

--- docs/js/firebase-config.js
+++ docs/js/firebase-config.js
@@ -4,7 +4,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { connectFirestoreEmulator, connectAuthEmulator, connectStorageEmulator, connectDatabaseEmulator } from "firebase/firestore";
+import { 
+    connectFirestoreEmulator,
+    connectAuthEmulator,
+    connectStorageEmulator,
+    connectDatabaseEmulator
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { ref, set } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
